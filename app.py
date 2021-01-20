964b9e5b5b7ef60a11244777a32e5861b740c4a8
ShilhoraAkshayPatel <Shilhora.akshay333@gmail.com>
2021-01-20 17:16:54 +0530

    second

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def predict():
     decoded = base64.b64decode(encoded)
     image = Image.open(io.BytesIO(decoded))
     processed_image = preprocess_image(image, target_size=(128, 128))
-
     prediction = model.predict(processed_image)
+    print(prediction)
     preclass = {0: "Apple_healthy", 1: "Apple_unhealthy", 2: "Pepper_bell_healthy", 3: "Pepper_bell_bacterial_spot", 4: "Cherry_(includingsor)_healthy", 5: "Cherry_(includingsor)_Powdery_mildew", 6: "Corn_(Maize)_healthy", 7: "Corn_(Maize)_unhealthy",
                 8: "Grape_healthy", 9: "Grape_unhealthy", 10: "Peach_healthy", 11: "Peach_bacterial_spot", 12: "Potato_healthy", 13: "Potato_unhealthy", 14: "Strawberry_healthy", 15: "Strawberry_Leaf_scorch", 16: "Tamato_healthy", 17: "Tamato_unhealthy"}
     response = preclass[np.argmax(prediction)]
--- readh.py
+++ readh.py
@@ -1,11 +1,3 @@
-import keras.models
-import tempfile
-import types
-import h5py
-import tensorflow as tf
-import pickle
-
-
 import base64
 import numpy as np
 import io
@@ -21,3 +13,35 @@ from flask import jsonify
 from flask import Flask
 import pickle
 import tensorflow as tf
+
+
+model = tf.keras.models.load_model("modelsav")
+
+
+def preprocess_image(image, target_size):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    return image
+
+
+def predict():
+
+    #encoded = message['image']
+    encoded = my_string
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(128, 128))
+
+    prediction = model.predict(processed_image)
+    print(prediction)
+    preclass = {0: "Apple_healthy", 1: "Apple_unhealthy", 2: "Pepper_bell_healthy", 3: "Pepper_bell_bacterial_spot", 4: "Cherry_(includingsor)_healthy", 5: "Cherry_(includingsor)_Powdery_mildew", 6: "Corn_(Maize)_healthy", 7: "Corn_(Maize)_unhealthy",
+                8: "Grape_healthy", 9: "Grape_unhealthy", 10: "Peach_healthy", 11: "Peach_bacterial_spot", 12: "Potato_healthy", 13: "Potato_unhealthy", 14: "Strawberry_healthy", 15: "Strawberry_Leaf_scorch", 16: "Tamato_healthy", 17: "Tamato_unhealthy"}
+    response = np.argmax(prediction)
+    return response
+
+
+execute = predict()
+print(execute)
