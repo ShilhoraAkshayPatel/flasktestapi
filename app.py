bf4002a250a92b848418c5df82983fcf9b9cdf27
ShilhoraAkshayPatel <Shilhora.akshay333@gmail.com>
2021-01-20 21:41:41 +0530

    thirdqqq

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ import base64
 import numpy as np
 import io
 from PIL import Image
-import keras
-from keras import backend as K
-from keras.models import Sequential
-from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing.image import img_to_array
 from flask import request
 from flask import jsonify
 from flask import Flask
@@ -22,7 +16,7 @@ def preprocess_image(image, target_size):
     if image.mode != "RGB":
         image = image.convert("RGB")
     image = image.resize(target_size)
-    image = img_to_array(image)
+    image = tf.keras.preprocessing.image.img_to_array(image)
     image = np.expand_dims(image, axis=0)
     return image
 
